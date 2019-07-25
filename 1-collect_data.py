8545b24da9a583642f5c22fb15e22073b332e19c
lfkopp <lfkopp@gmail.com>
2019-07-24 21:49:30 -0300

    atualizando

---
+++ 1-collect_data.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import requests
+import json
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+# In[2]:
+
+
+options = 'results=8000&timezone=America%2FSao_Paulo'
+data = requests.get('https://api.thingspeak.com/channels/657211/feeds.json?'+options).content
+feeds = json.loads(data)['feeds']
+display(feeds[0])
+display(feeds[-1])
+
+
+# In[3]:
+
+
+df = pd.DataFrame.from_dict(feeds)
+fields = [x for x in df.columns if x.startswith('field')]
+for field in fields:
+    df[field] = df[field].str.replace('\r\n','').astype(float)
+df['created_at'] = pd.to_datetime(df['created_at'])
+
+
+# In[4]:
+
+
+df.tail()
+
+
+# In[5]:
+
+
+df2 =  df.set_index('created_at').resample("5T").mean()[fields]
+
+
+# In[6]:
+
+
+df_old = pd.read_pickle('data.pickle')
+df_old = df_old.append(df2)
+df_old = df_old.drop_duplicates().resample("5T").mean()[fields]
+
+
+# In[7]:
+
+
+df_old.tail()
+
+
+# In[8]:
+
+
+df_old[['field2','field4']].plot(figsize=(15,10),)
+
+
+# In[9]:
+
+
+df_old[['field1','field3']].plot(figsize=(15,10))
+
+
+# In[10]:
+
+
+df_old.to_pickle('data.pickle')
+
+
+# In[11]:
+
+
+dfx = df_old.reset_index()
+#dfx.interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
+dfx['date2'] = dfx['created_at'].dt.hour + dfx['created_at'].dt.minute/60 + dfx['created_at'].dt.second/3600
+dfx.tail()
+
+
+# In[12]:
+
+
+limits = [(int(dfx[['field1','field3']].min().min()),int(dfx[['field1','field3']].max().max()+1)),
+          (int(dfx[['field2','field4']].min().min()),int(dfx[['field2','field4']].max().max())+1)]
+limits
+
+
+# In[13]:
+
+
+dfx['theta'] = dfx['date2']  * np.pi / 24. * 2
+last = 200
+
+fig = plt.figure(figsize=(20,30))
+
+## Temperature
+ax = fig.add_subplot(1,2,1,projection='polar')
+ax.set_xticklabels(['0h', '3h', '6h', '9h', '', '15h', '18h', '21h'])
+plt.title('Temperature')
+plt.ylabel('Celsius')
+ax.plot(dfx['theta'] , dfx['field1'], alpha=.2 ,color='blue' )
+ax.plot(dfx['theta'] , dfx['field3'], alpha=.2,color='red' )
+ax.plot(dfx['theta'][-last:] , dfx['field1'][-last:], alpha=.8,lw=1.6 ,color='blue')
+ax.plot(dfx['theta'][-last:] , dfx['field3'][-last:], alpha=.8,lw=1.6 ,color='red' )
+plt.ylim(limits[0])
+ax.legend(('quarto','cozinha'))
+
+## Humidity
+ax = fig.add_subplot(1,2,2,projection='polar')
+ax.set_xticklabels(['0h', '3h', '6h', '9h', '', '15h', '18h', '21h'])
+plt.title('Humidity')
+plt.ylabel('% moisture')
+ax.plot(dfx['theta'] , dfx['field2'], alpha=.2 ,color='blue' )
+ax.plot(dfx['theta'] , dfx['field4'], alpha=.2,color='red' )
+ax.plot(dfx['theta'][-last:] , dfx['field2'][-last:], alpha=.8,lw=1.6 ,color='blue')
+ax.plot(dfx['theta'][-last:] , dfx['field4'][-last:], alpha=.8,lw=1.6 ,color='red' )
+plt.ylim(limits[1])
+ax.legend(('quarto','cozinha'))
+
+plt.savefig('plot.png',dpi=96, bbox_inches = 'tight')
+plt.show()
+
+
+# In[14]:
+
+
+dfx.describe()
+
+
+# In[15]:
+
+
+d2 = dfx[['created_at','field1']].groupby(dfx['created_at'].dt.date)
+fig = plt.figure(figsize=(20,30))
+ax = fig.add_subplot(1,1,1)
+
+for d in d2:
+    if str(d[0]) == str(datetime.now().strftime("%Y-%m-%d")):
+        ax.plot(d[1]['created_at'].dt.time , d[1]['field1'], alpha=1 ,color='red' )
+    else:
+        ax.plot(d[1]['created_at'].dt.time , d[1]['field1'], alpha=.2 ,color='blue' )
+
+plt.savefig('plot_field1.png',dpi=96, bbox_inches = 'tight')
+plt.show()
+
+
+# In[16]:
+
+
+d2 = dfx[['created_at','field3']].groupby(dfx['created_at'].dt.date)
+fig = plt.figure(figsize=(20,30))
+ax = fig.add_subplot(1,1,1)
+
+for d in d2:
+    if str(d[0]) == str(datetime.now().strftime("%Y-%m-%d")):
+        ax.plot(d[1]['created_at'].dt.time , d[1]['field3'], alpha=1 ,color='red' )
+    else:
+        ax.plot(d[1]['created_at'].dt.time , d[1]['field3'], alpha=.2 ,color='blue' )
+plt.savefig('plot_field3.png',dpi=96, bbox_inches = 'tight')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
